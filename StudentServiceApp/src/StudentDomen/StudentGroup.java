a36797145f552d9d26fdebd62e289be467c016d6
Badri <9299547077@mail.ru>
2023-04-29 22:18:18 +0300

    HW_3_upDate_to_HW_4

--- StudentServiceApp/src/StudentDomen/StudentGroup.java
+++ StudentServiceApp/src/StudentDomen/StudentGroup.java
@@ -49,33 +49,6 @@ public class StudentGroup implements Iterable<Student>,  Comparable<StudentGroup
         return Integer.compare(students.size(), o.students.size());
     }
 
-//     import java.util.Collections;
 
-// public class Main {
-
-//     public static void main(String[] args) {
-//         StudentStream stream = new StudentStream(1);
-
-//         StudentGroup group1 = new StudentGroup("Group 1");
-//         group1.addStudent(new Student("Student 1"));
-//         group1.addStudent(new Student("Student 2"));
-
-//         StudentGroup group2 = new StudentGroup("Group 2");
-//         group2.addStudent(new Student("Student 3"));
-//         group2.addStudent(new Student("Student 4"));
-//         group2.addStudent(new Student("Student 5"));
-
-//         stream.addGroup(group1);
-//         stream.addGroup(group2);
-
-//         Collections.sort(stream);
-
-//         for (StudentGroup group : stream) {
-//             System.out.println("Group: " + group.getFirstName() + " (" + group.students.size() + " students)");
-//             for (Student student : group) {
-//                 System.out.println(" - " + student.getFirstName());
-//             }
-//         }
-    // }
     
 }
